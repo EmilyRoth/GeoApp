e9cff419ff176830df0785b5d8739a8b815902db
Emily Lauren Roth <emilylroth@utexas.edu>
2018-10-24 16:17:27 -0500

    API Response working

--- app/src/main/java/com/example/geoapp/MainActivity.java
+++ app/src/main/java/com/example/geoapp/MainActivity.java
@@ -13,6 +13,7 @@ public class MainActivity extends AppCompatActivity {
     private static String address;
     Button nextButton;
     static EditText input;
+    public final static String ADDRESS = "";
 // https://maps.googleapis.com/maps/api/geocode/json?address=1600+Amphitheatre+Parkway,+Mountain+View,+CA&key=YOUR_API_KEY
     @Override
     protected void onCreate(Bundle savedInstanceState) {
--- app/src/main/java/com/example/geoapp/mapActivity.java
+++ app/src/main/java/com/example/geoapp/mapActivity.java
@@ -6,21 +6,50 @@ import android.os.Bundle;
 import android.widget.Button;
 import android.widget.TextView;
 
-import com.google.android.gms.maps.GoogleMap;
-import com.google.android.gms.maps.SupportMapFragment;
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+import com.google.maps.GaeRequestHandler;
+import com.google.maps.GeoApiContext;
+import com.google.maps.GeocodingApi;
+import com.google.maps.errors.ApiException;
+import com.google.maps.model.GeocodingResult;
+
+import java.io.IOException;
+
+//import com.google.android.gms.maps.GoogleMap;
+//import com.google.android.gms.maps.SupportMapFragment;
 
 public class mapActivity extends AppCompatActivity {
 
-    private GoogleMap mMap;
+    private static final String apiKey = "AIzaSyA-3fRqWzxM3VjT9krfB3U0WFgI6utSNAI";
+    //  private GoogleMap mMap;
     // takes in the next activity to do the map
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-      //  SupportMapFragment map = ((SupportMapFragment) getSupportFragmentManager().findFragmentById(R.id.map));
-        Intent intent = getIntent();
-        String address = intent.getStringExtra("address");
-        TextView tv = findViewById(R.id.textView);
-        tv.setText(address);
         setContentView(R.layout.activity_map);
+
+        //  SupportMapFragment map = ((SupportMapFragment) getSupportFragmentManager().findFragmentById(R.id.map));
+        Intent intent = getIntent();
+        String address = intent.getExtras().getString("address");
+        TextView tv = findViewById(R.id.tv_address);
+
+        GeoApiContext context = new GeoApiContext.Builder().apiKey(apiKey).build();
+        GeocodingResult[] results = new GeocodingResult[0];
+        try {
+            results = GeocodingApi.geocode(context,
+                    "1600 Amphitheatre Parkway Mountain View, CA 94043").await();
+        } catch (ApiException e) {
+            e.printStackTrace();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        Gson gson = new GsonBuilder().setPrettyPrinting().create();
+        System.out.println(gson.toJson(results[0].addressComponents));
+
+        tv.setText(gson.toJson(results[0].addressComponents));
+
     }
 }
